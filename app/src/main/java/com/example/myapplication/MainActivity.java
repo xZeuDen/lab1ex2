81d0e1610736f7086f757f53ad11b90d5cee5b47
xZeuDen <162120589+xZeuDen@users.noreply.github.com>
2026-09-25 14:04:24 +0100

    Added validation for phone field and name field

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -27,9 +27,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void button(View view) {
-        EditText name = findViewById(R.id.name);
-        String txt = name.getText().toString();
 
-        Toast.makeText(this, "Thank you for submitting your response " + txt, Toast.LENGTH_SHORT).show();
+        //Find the fields by id
+        EditText name = findViewById(R.id.name);
+        EditText password = findViewById(R.id.password);
+        EditText email = findViewById(R.id.email);
+        EditText phone = findViewById(R.id.phone);
+
+        //Parse the input text from text to strings
+        String txtName = name.getText().toString();
+        String txtPassword = password.getText().toString();
+        String txtEmail = email.getText().toString();
+        String txtPhone = phone.getText().toString();
+
+
+        if(txtPhone.matches("[a-zA-Z]+")) {
+            phone.setError("Phone must only contain numbers");
+            phone.requestFocus();
+            return;
+        }
+        if(!txtName.matches("[a-zA-Z]+")) {
+            name.setError("Name must only contain letters");
+            name.requestFocus();
+            return;
+        }
+        Toast.makeText(this, "Thank you for submitting your response " + txtName, Toast.LENGTH_SHORT).show();
     }
 }
